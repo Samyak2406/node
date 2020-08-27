e05e27c6cf5c369e93d8b32146db9fd7e74e8088
Samyak2406 <samyak.vce18@sot.pdpu.ac.in>
2020-08-27 14:34:58 +0530

    Added stttic wwebpages to server.

--- node-express/index.js
+++ node-express/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const http = require("http");
-
+const morgan = require('morgan');
 const hostName = "localhost";
 const port = 3000;
 
 const app = express();
-
+app.use(morgan("dev"))
+app.use(express.static(__dirname+'/public'));
 app.use((req,res,next) =>{// Called to set up the server
-    console.log(req.headers);
+    // console.log(req.headers);
     res.statusCode = 200;
     res.setHeader("Content-Type","text/html");
     res.end("Express Server is Set");
